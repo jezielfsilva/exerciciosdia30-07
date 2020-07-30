230f27bb2c36c7a95c1359b2162c25205709c935
vainawebm <vainaweb@gmail.com>
2020-07-30 16:02:09 -0300

    novos arquivos criados

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Crie uma lista com suas musicas preferidas, e mostre essa lista utilizando forEach. //
+
+// Crie uma lista com o nome de seus amigos, e mostre essa lista utilizando map. //
+
+// Crie uma condição que verifeque se você tem mais de 5 músicas preferidas. //
+
+
+// Crie uma função com as seguintes características: //
+// 1. A função deve receber 3 argumentos; //
+
+// 2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string: //
+   // "Preencha todos os valores corretamente!" //
+   
+// 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação. //
