46cd893dbe8e7c05b71ec9a242d7d395dfac8233
vainawebm <vainaweb@gmail.com>
2020-07-30 16:33:28 -0300

    último exercício em andamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,31 @@
 // Crie uma lista com suas musicas preferidas, e mostre essa lista utilizando forEach. //
 
+var favoriteMusic = ['The Scientist', 'Back At One', 'Light', 'Photograph', 'Ironic'];
+favoriteMusic.forEach(music => console.log(music));
+
 // Crie uma lista com o nome de seus amigos, e mostre essa lista utilizando map. //
+
+var friends = ['Edison', 'Jorge', 'David', 'Lucas', 'Carlos Henrique', 'Danilo'];
+friends.map(person => console.log(person));
 
 // Crie uma condição que verifeque se você tem mais de 5 músicas preferidas. //
 
+if(favoriteMusic.length > 5) {
+    console.log('legal tenho mais que 5 músicas');
+} else {
+    console.log('Não só tenho 5 músicas');
+}
 
-// Crie uma função com as seguintes características: //
-// 1. A função deve receber 3 argumentos; //
+/* Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    "Preencha todos os valores corretamente!"
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação. */
 
-// 2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string: //
-   // "Preencha todos os valores corretamente!" //
-   
-// 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação. //
+var countries = 38;
+var states = 'dezessete';
+var cities = 74;
+
+const world = (countries, states, cities) => {
+    
+};
